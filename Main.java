855d1b6c46e0e3aa5c859e5fd59e253933367a75
KSV7 <93730979+KSV7@users.noreply.github.com>
2021-12-05 19:00:55 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,23 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
+		Cat a = new Cat("Umka", 12);
+		Cat b = new Cat("Luska", 5);
+		Cat c = new Cat("Tom", 2);
+		
+		Cat d = new Cat("Umka", 12);
+		Cat e = new Cat("Barsic", 12);
+
+		// Проверка на корректность реализации Comparable
+		System.out.println(sgn(a.compareTo(b)) == -sgn(b.compareTo(a)));
+		System.out.println((b.compareTo(a) > 0 && b.compareTo(d) > 0) && a.compareTo(c) > 0);
+		System.out.println(a.compareTo(d) == 0 && (sgn(a.compareTo(c)) == sgn(d.compareTo(c))));
+
+		// Проверка на согласованность с equals
+		System.out.println(a.equals(d) == true && a.compareTo(d) == 0);
+		System.out.println(a.equals(b) == false && a.compareTo(b) != 0);
+		System.out.println(a.equals(e) == false && a.compareTo(e) != 0);
+		
 		Cat cat7 = new Cat("Garfild", 4);
 		Cat cat1 = new Cat("Umka", 12);
 		Cat cat2 = new Cat("Luska", 5);
